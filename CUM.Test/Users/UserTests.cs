6df6c68f28c307f91be51cd34fc9432d8f98b87e
Tony Ochiche <aochiche@msn.com>
2021-02-09 10:37:19 -0600

    Add Test class and Failing test...

---
+++ CUM.Test/Users/UserTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace CUM.Test.Users
+{
+    public class UserTests
+    {
+
+
+        #region Get By Id
+
+        [Fact]
+        public async void Task_GetUserById_Return_OkResult()
+        {
+            // Arrange the data
+            var controller = new UserController();
+            var userId = 1;
+
+            // Performing the data
+            var data = await controller.GetPost(userId);
+
+            // Matching the ouput (Arrange, Act, Assert)
+            Assert.IsType<OkObjectResult>(data);
+        }
+
+        #endregion
+    }
+}
