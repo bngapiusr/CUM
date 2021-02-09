f4489a9e48eb375871264b41064bf8573751d11d
Tony Ochiche <aochiche@msn.com>
2021-02-09 12:48:20 -0600

    Add context class to Data project and add support for EF core inmemory db...

---
+++ CUM.Data/CumContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.Common;
+using CUM.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CUM.Data
+{
+    public class CumContext : DbContext 
+    {
+        public DbSet<User> User { get; set; }
+        public CumContext(DbContextOptions<CumContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>(e =>
+            {
+                e.ToTable("user")
+                    .HasKey(u => u.UserId);
+                e.Property(p => p.UserId)
+                    .ValueGeneratedOnAdd();
+            });
+        }
+    }
+}   
